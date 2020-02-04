e73789edbbebba93fe155df6cd23b99c929385d6
Trineas <danny.sternbeck@gmail.com>
2020-02-04 15:25:08 +0100

    Unity Project Initial commit

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public GameObject cpOn, cpOff;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GameManager.instance.SetSpawnPoint(transform.position);
+
+            Checkpoint[] allCP = FindObjectsOfType<Checkpoint>();
+            for (int i = 0; i < allCP.Length; i++)
+            {
+                allCP[i].cpOff.SetActive(true);
+                allCP[i].cpOn.SetActive(false);
+            }
+
+            cpOff.SetActive(false);
+            cpOn.SetActive(true);
+        }
+    }
+}
