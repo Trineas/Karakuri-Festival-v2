462d2afa943a4af3e21b59762f2dc5120854d8bf
Trineas <danny.sternbeck@gmail.com>
2020-07-16 16:45:53 +0200

    Bomb Effect adjusted

--- Assets/Scripts/LevelMechanics/MapUnlockTrigger.cs
+++ Assets/Scripts/LevelMechanics/MapUnlockTrigger.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class MapUnlockTrigger : MonoBehaviour
 {
+    public int soundToplay;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
+            AudioManager.instance.PlaySFX(soundToplay);
             GameManager.instance.mapUnlocked = true;
             Destroy(this.gameObject);
         }
